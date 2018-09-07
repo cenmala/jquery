46ab880272ab7a97c917586e303b5ad24324b7a9
Cesar Manrique <cmanrique@udes.edu.co>
2018-09-07 18:15:49 -0500

    Segundo commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var nombre = 'Cesar Manrique';
+/*
+enteros
+flotantes
+cadenas de texto
+elementos de html
+objetos
+*/
+
+/*
+ + - * / Operadores aritmeticos
+ ++ -- Aumentar uno u restar uno
+ = Asignacion
+ < > == != <= >= % Comparacion entre elementos
+ && || ! Logicos
+*/
+alert('hola mundo');
