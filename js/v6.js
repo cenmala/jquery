fb12dba381e835a05b43febf7e12137303987d35
Cesar Manrique <cenmala@gmail.com>
2018-09-10 22:10:18 -0500

    + Se hizo el ejercicio del video 6 + Se mejora navegacion de paginas

---
+++ js/v6.js
@@ -0,0 +1,21 @@
+// alert('Hola los saluda Cesar desde v6.j6');
+
+// var saludo = $('.saludo').html();
+// alert(saludo);
+
+var clase = $('h1').attr('class');
+alert(clase);
+
+$('h1').attr('class','h1_saludo');
+var claseNew = $('h1').attr('class');
+alert(claseNew);
+
+var idp = $('p').attr('id');
+alert(idp);
+
+$('p').attr('id','parrafo_nuevo');
+var idpNew = $('p').attr('id');
+alert(idpNew);
+
+$('.h1_saludo').attr('class','parrafo');
+$('#parrafo_nuevo').attr('id','saludo');
