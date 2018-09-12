bbf9b48cf76eec0162a3a2e3a67140eb13c807e9
Cesar Manrique <cmanrique@udes.edu.co>
2018-09-12 18:21:03 -0500

    +Se terminó el ejercicio 9

--- js/v9.js
+++ js/v9.js
@@ -1,11 +1,16 @@
 $(function(){
+	// show and hide
+
 	$('.saludo').css({'background-color':'DodgerBlue','text-align':'center'});
+	
 	if($('#parrafo').hasClass('saludo')){
-		alert('Si tiene la clase Saludo');
+		alert('Si tiene la clase saludo y ocultar pero con show lo mostramos');
 		$('#parrafo').removeClass('saludo');
+		$('#parrafo').show(2000)
 	}else{
 		alert('No tiene la clase saludo');
 		$('div').addClass('contenedor');
-		$('#parrafo').addClass('saludo');
+		$('div').hide(2000);
+		// $('#parrafo').addClass('saludo');
 	}
 });
