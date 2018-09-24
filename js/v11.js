0fe5b0ead753d942051025f1e02ca7d35a47cf47
Cesar Manrique <cmanrique@udes.edu.co>
2018-09-24 14:45:51 -0500

    +Se modifica el 11 para recorrerlo con un for sin ir uno por uno en el arreglo de los colores.

--- js/v11.js
+++ js/v11.js
@@ -1,10 +1,10 @@
 $(function(){
 	// Iterar elementos con for of
 	var numbers = [1,2,3,4,5,6,7,8,9,0];
-	alert("Tamaño arregle numbers: " + numbers.length);
+	console.log("Tamaño arregle numbers: " + numbers.length);
 
 	for (let num of numbers) {
-		alert(num);
+		// alert(num);
 		console.log(num);
 	}
 
@@ -28,12 +28,22 @@ $(function(){
 	var r = 2;
 
 	for (let color of colors) {
-		alert("Nombre Color: "+color.colors[p].colorName+" "+"Código Hexa Color: "+color.colors[p].colorHexa);
-		alert("Nombre Color: "+color.colors[q].colorName+" "+"Código Hexa Color: "+color.colors[q].colorHexa);
-		alert("Nombre Color: "+color.colors[r].colorName+" "+"Código Hexa Color: "+color.colors[r].colorHexa);
+		// alert("Nombre Color: "+color.colors[p].colorName+" "+"Código Hexa Color: "+color.colors[p].colorHexa);
+		// alert("Nombre Color: "+color.colors[q].colorName+" "+"Código Hexa Color: "+color.colors[q].colorHexa);
+		// alert("Nombre Color: "+color.colors[r].colorName+" "+"Código Hexa Color: "+color.colors[r].colorHexa);
 		console.log("Nombre Color: "+color.colors[p].colorName+" "+"Código Hexa Color: "+color.colors[p].colorHexa);
 		console.log("Nombre Color: "+color.colors[q].colorName+" "+"Código Hexa Color: "+color.colors[q].colorHexa);
 		console.log("Nombre Color: "+color.colors[r].colorName+" "+"Código Hexa Color: "+color.colors[r].colorHexa);
 		
 	}
+
+	console.log("Arreglo de "+colors.length);
+
+	for (var i = 0; i < 3; i++) {
+		// console.log(i);
+		for (let color of colors) {
+			console.log("Nombre Color: "+color.colors[i].colorName+" "+"Código Hexa Color: "+color.colors[i].colorHexa);
+		}
+	}
+
 });
