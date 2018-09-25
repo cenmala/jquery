afeed642e157603c895dc409f45bde3f1ccf4cf9
Cesar Manrique <cmanrique@udes.edu.co>
2018-09-25 12:00:52 -0500

    +Se hacen cambios para revisar que pasa en cada evento cuando se llama cuentaCaracteres

--- js/v15.js
+++ js/v15.js
@@ -1,28 +1,37 @@
 $(function(){
 	//metodos de teclado
 	console.log('Entra al js');
-	
+	var i = 0;
+
 	$("#textbox1").keydown(function(){
 		console.log('Entra al keydown');
 		$("#textbox1").css({"background-color":"#ccffcc"});
+		// cuentaCaracteres($("#textbox1").val());
 	});
 
 	
 	$("#textbox1").keyup(function(){
 		console.log('Entra al keyup');
 		$("#textbox1").css({"background-color":"#ffffff"});
+		cuentaCaracteres($("#textbox1").val());
 	});
 
-	var i = 0;
 	$("#textbox1").keypress(function(){
 		console.log('Entra al keypress');
+		$("#spam-numero").text(i += 1);
+		// cuentaCaracteres($("#textbox1").val());
+	});
+
+	$("#textbox1").change(function(){
+		console.log('Entra al change');
 		$("#spam-numero").text(i += 1);
 		cuentaCaracteres($("#textbox1").val());
 	});
 
 	function cuentaCaracteres(cadena){
-		var j = cadena.length;
-		j = j+1;
-		console.log("La cadena mide: "+j);
+		//se puede revisar lo que pasa cuando se llama en el keydown y en el keypress
+		console.log(cadena);
+		console.log(cadena.length);
+
 	};
 });
