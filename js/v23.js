8ad68715350ddef8c8bb85b159900d9aad1e2379
Cesar Manrique <cenmala@gmail.com>
2018-10-05 10:03:25 -0500

    +Se termina ejercicio del video 23

--- js/v23.js
+++ js/v23.js
@@ -1,38 +1,12 @@
 $(function(){
-	console.log('Entra al js 22');
-	var i = 0;
+	console.log('Entra al js 23');
 
-	$('#exe1').click(function(){
-		$('ol').append('hola');
-	});
-
-	$('#exe2').click(function(){
-		i++;
-		$('ol').append('<li>Texto '+i+' insertado con jQuery</li>');
-	});
-
-	$('#exe3').click(function(){
-		i++;
-		$('ol').prepend('<li>Texto '+i+' insertado con jQuery</li>');
-	});
-
-	$('#exe4').click(function(){
-		$('ol').append($('ol li:first'));
-	});
-
-	$('#exe5').click(function(){
-		$('ol').prepend($('ol li:last'));
-	});
-
-	$('#exe6').click(function(){
-		var clon = $('ol li:last').text();
-		console.log(clon);
-		$('#clonado').append(clon);
-		$('ol').prepend($('ol li:last'));
-	});
-
-	$('#exe7').click(function(){
-		$('ol li:last').remove();
-	});
-
+	 $("#informacion").click(function(){
+		 var hijos = $('#padre').children().text();
+		 var padre = $('.hijo').parent();
+		 var hermanos = $('.hijo').siblings().text();
+		 console.log('Los hijos son: '+hijos);
+		 console.log(padre);
+		 console.log('Los hermanos son: '+hermanos);
+	 });
 });
